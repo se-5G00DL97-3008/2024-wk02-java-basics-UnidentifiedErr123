067ff2cdc65ac51f2a147f0acd5213a4af2466b0
UnidentifiedErr123 <ducanhle797@gmail.com>
2024-01-17 20:51:27 +0300

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 class E02 {
     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
 
+        // Ask the user for the first number
+        System.out.println("Give a number:");
+        double firstNumber = scanner.nextDouble();
+
+        // Ask the user for the second number
+        System.out.println("Give another number:");
+        double secondNumber = scanner.nextDouble();
+
+        // Perform calculations
+        double sum = firstNumber + secondNumber;
+        double product = firstNumber * secondNumber;
+        double difference = firstNumber - secondNumber;
+        double quotient = firstNumber / secondNumber;
+
+        // Print results
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+        System.out.println(firstNumber + " * " + secondNumber + " = " + product);
+        System.out.println(firstNumber + " - " + secondNumber + " = " + difference);
+        System.out.println(firstNumber + " / " + secondNumber + " = " + quotient);
+
+        // Close the scanner
+        scanner.close();
     }
 }
