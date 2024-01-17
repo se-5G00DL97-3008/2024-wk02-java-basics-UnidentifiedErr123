3bce4a1c72a5163b7e53e600d92d3f08053217af
UnidentifiedErr123 <ducanhle797@gmail.com>
2024-01-17 20:54:17 +0300

    Update E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 class E04 {
     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
 
+        // Ask the user to enter a day
+        System.out.println("Enter a day:");
+        int day = scanner.nextInt();
+
+        // Ask the user to enter a month
+        System.out.println("Enter a month:");
+        int month = scanner.nextInt();
+
+        // Check if it's Christmas
+        if (day == 24 && month == 12) {
+            System.out.println("Merry Christmas!");
+        } else {
+            System.out.println("No special wish for today.");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
