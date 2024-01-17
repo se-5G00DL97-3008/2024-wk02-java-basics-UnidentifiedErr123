8078cda04ae64e4ae825b6cdd13bcf1df2cf28c0
UnidentifiedErr123 <ducanhle797@gmail.com>
2024-01-17 20:49:32 +0300

    Update E01.java

--- e01/E01.java
+++ e01/E01.java
@@ -1,5 +1,24 @@
 class E01 {
     public static void main(String[] args) {
+         // Declare and initialize variables
+        int x = 10;
+        float y = 5.5f;
+        boolean flag = true;
+        char letter = 'c';
+        short year = 2022;
+        long population = 7000000000L;
+        byte num = 100;
+        double pi = 3.1345759992338476;
+
+        // Print variables to the console
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(flag);
+        System.out.println(letter);
+        System.out.println(year);
+        System.out.println(population);
+        System.out.println(num);
+        System.out.println(pi);
 
     }
 }
