39cf92bbdad241de6d52dc158fe59ec004a7384f
UnidentifiedErr123 <ducanhle797@gmail.com>
2024-01-17 20:55:21 +0300

    Update E05.java

--- e05/E05.java
+++ e05/E05.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
 class E05 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Ask the user to enter a number
+        System.out.println("Enter a number between 1 and 7:");
+        int dayNumber = scanner.nextInt();
+
+        // Use a switch statement to determine the day of the week
+        switch (dayNumber) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Only 7 days in a week");
+                break;
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
